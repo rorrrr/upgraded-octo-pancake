a42039ce359d9eba6287ad62248cfa8aa33d21ee
Rory MacDonald <rorymacdonald6@hotmail.com>
2017-02-20 03:50:51 +0000

    all done and it works, wow

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 window.onload = function() {
 
-  var url = 'https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image,info,lore,stats&api_key=RGAPI-74b55f1b-abee-4a11-9b31-dc052253a8fd';
+  var url = 'https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=image,info,lore,skins,stats&api_key=RGAPI-74b55f1b-abee-4a11-9b31-dc052253a8fd';
   var request = new XMLHttpRequest();
   request.open("GET", url);
 
@@ -10,131 +10,164 @@ window.onload = function() {
       var jsonString = request.responseText;
       var champions = JSON.parse(jsonString).data;
       main(champions);
-    }
+      //   var changeBGImage = function(){
+        //         document.body.background = "http://na.leagueoflegends.com/sites/default/files/styles/scale_large/public/upload/art/teambuilder-wallpaper.jpg?itok=-H7_WlsZ)";
+        //    }
+
+        //    changeBGImage();
+      }
+    };
+    request.send();
+
   };
-  request.send();
-
-};
 
 
-var main = function (champions) {
+  var main = function (champions) {
 
-  // var championNames = JSON.parse(responseText).data;
-  populateSelect(champions);
+    // var championNames = JSON.parse(responseText).data;
+    populateSelect(champions);
 
-  var championsDeets = [];
+    var championsDeets = [];
 
-  for (championObject in champions){
+    for (championObject in champions){
 
-    championsDeets.push(
-    {
-      key: championObject,
-      name: champions[championObject]['name'],
-      title: champions[championObject]['title'],
-      image: champions[championObject]['image'],
-      lore: champions[championObject]['lore'],
-      info: champions[championObject]['info']
-    })
+      championsDeets.push(
+      {
+        key: championObject,
+        name: champions[championObject]['name'],
+        title: champions[championObject]['title'],
+        image: champions[championObject]['image'],
+        lore: champions[championObject]['lore'],
+        info: champions[championObject]['info'],
+        skins: champions[championObject]['skins']
+      })
+    }
+
+
+
+    var cached = localStorage.getItem("selectedChampion");
+    var selected = champions[0];
+    // console.log(cached)
+
+    if(cached) {
+      selected = JSON.parse(cached);
+      var selector = document.querySelector('#champions').selectedIndex = selected.index +1;
+      // updateDisplay(selected);
+
+    }
+    document.querySelector('#details').style.display = 'block';
   }
 
+  var populateSelect = function (champions) {
+    var parent = document.querySelector('#champions');
 
-  
-  var cached = localStorage.getItem("selectedChampion");
-  var selected = champions[0];
-  // console.log(cached)
+    var championsDeets = [];
 
-  if(cached) {
-    selected = JSON.parse(cached);
-    var selector = document.querySelector('#champions').selectedIndex = selected.index +1;
-    // updateDisplay(selected);
+    for (championObject in champions){
 
-  }
-  document.querySelector('#details').style.display = 'block';
-}
-
-var populateSelect = function (champions) {
-  var parent = document.querySelector('#champions');
-
-  var championsDeets = [];
-
-  for (championObject in champions){
-
-    championsDeets.push(
-    {
-      key: championObject,
-      name: champions[championObject]['name'],
-      title: champions[championObject]['title'],
-      image: champions[championObject]['image'],
-      lore: champions[championObject]['lore'],
-      info: champions[championObject]['info']
-    })
-  }
+      championsDeets.push(
+      {
+        key: championObject,
+        name: champions[championObject]['name'],
+        title: champions[championObject]['title'],
+        image: champions[championObject]['image'],
+        lore: champions[championObject]['lore'],
+        info: champions[championObject]['info'],
+        skins: champions[championObject]['skins']
+      })
+    }
 
 
 
-  championsDeets.forEach(function (item, index) {
-    item.index = index;
-    var option = document.createElement("option");
-    option.value = index.toString();
-    option.text = item.name;
-    parent.appendChild(option);
-  });
+    championsDeets.forEach(function (item, index) {
+      item.index = index;
+      var option = document.createElement("option");
+      option.value = index.toString();
+      option.text = item.name;
+      parent.appendChild(option);
+    });
 
-  parent.style.display = 'block';
+    parent.style.display = 'block';
 
-  parent.addEventListener('change', function(event) {
-    var index = this.value;
-    console.log(index)
-    var champion = championsDeets[index];
-    console.log(champion)
-    updateDisplay(champion);
-    localStorage.setItem("selectedChampion",JSON.stringify(champion));
-  });
+    parent.addEventListener('change', function(event) {
+      var index = this.value;
+      var champion = championsDeets[index];
+      updateDisplay(champion);
+      localStorage.setItem("selectedChampion",JSON.stringify(champion));
+    });
 
-};
+  };
 
-var updateDisplay = function(deets) {
-  var tags = document.querySelectorAll('#details *');
-  // console.log(deets)
-  tags[0].innerText = deets.name;
-  tags[1].innerText = deets.title;
-  tags[2].innerHTML = "<h3>LORE: </h3>" + deets.lore;
-};
+  var updateDisplay = function(deets) {
+    var tags = document.querySelectorAll('#details *');
+    var champImg = "http://ddragon.leagueoflegends.com/cdn/7.3.3/img/champion/" + deets.image.full;
+    var skinCount = 0;
+
+    for(skin in deets.skins){
+      skinCount++;
+    }
+    var skinNumber;
+    var skinPicker = function(skinCount){
+      skinNumber = Math.floor((Math.random() * (skinCount)) + 0);
+    }
+    skinPicker(skinCount)
+
+    var randomSkinImg ="http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" +deets.image.full.slice(0, -4) +"_" + skinNumber +".jpg"
+    console.log(randomSkinImg)
+    tags[0].innerHTML = deets.name;
+    tags[1].src = champImg;
+    tags[2].innerText = deets.title;
+    tags[3].innerHTML = "LORE:   " + deets.lore;
+
+
+    var changeBGImage = function(){
+     document.body.background = randomSkinImg;
+   }
+
+   changeBGImage();
+ };
+
+ // var updateBackground = function(deets) {
+  //   var background = document.getElementById('#background');
+
+  // }
+
+  // var displayImg = function()
+  // + " " + "<img src =" + img + ">";
 
 
 
 
+  // var updateDisplay = function(details) {
+    //   var tags = document.querySelectorAll('#champions');
+    //   tags[0].innerText = details['name'];
+    //   tags[1].innerText = details['title'];
+    //   tags[2].innerText = details['lore'];
+    // };
 
-// var updateDisplay = function(details) {
-  //   var tags = document.querySelectorAll('#champions');
-  //   tags[0].innerText = details['name'];
-  //   tags[1].innerText = details['title'];
-  //   tags[2].innerText = details['lore'];
-  // };
 
+    // var loadChampionList = function(responseText) {
+      //   var championNames = JSON.parse(responseText).data;
 
-  // var loadChampionList = function(responseText) {
-    //   var championNames = JSON.parse(responseText).data;
+      //   var nameList = Object.keys(championNames);
 
-    //   var nameList = Object.keys(championNames);
+      //   // console.log(championNames)
+      //   championDeets = [];
 
-    //   // console.log(championNames)
-    //   championDeets = [];
+      //   for (championName in championNames){
 
-    //   for (championName in championNames){
+        //     championDeets.push(
+        //     {
+          //       // key: champion,
+          //       name: championNames[championName]['name'],
+          //       title: championNames[championName]['title'],
+          //       image: championNames[championName]['image'],
+          //       lore: championNames[championName]['lore'],
+          //       info: championNames[championName]['info']
+          //     })
+          //   }
 
-      //     championDeets.push(
-      //     {
-        //       // key: champion,
-        //       name: championNames[championName]['name'],
-        //       title: championNames[championName]['title'],
-        //       image: championNames[championName]['image'],
-        //       lore: championNames[championName]['lore'],
-        //       info: championNames[championName]['info']
-        //     })
-        //   }
-
-        // console.log(championDeets)
+          // console.log(championDeets)
 
 
 
